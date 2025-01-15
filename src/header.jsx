bd358e674ad19520c5f8fce30be8458dfa8b6bdc
Jack <jackdouma0@gmail.com>
2025-01-15 16:15:00 -0500

    temp front end

---
+++ src/header.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './header.css'
+
+function Header() {
+
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <div class="right">
+                    <li><a href="#"><i class="fas fa-sign-in-alt"></i>Login</a></li>
+                    <li><a href="#"><i class="fas fa-user-plus"></i>Sign Up</a></li>
+                </div>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,21 @@ import viteLogo from '/vite.svg'
 function Index() {
 
   return (
-    <div id="root">
-        <h1>Welcome to WagerWorld</h1>
-    </div>
+    <main>
+        <h1>WagerWorld</h1>
+        <section class="roomContainer">
+            <div class="createRoom">
+                <h2>Create Room</h2>
+                <input type="text" placeholder="Enter room name"></input>
+                <button>Create Room</button>
+            </div>
+            <div class="joinRoom">
+                <h2>Join Room</h2>
+                <input type="text" placeholder="Enter code"></input>
+                <button>Join Room</button>
+            </div>
+        </section>
+    </main>
   )
 }
 
