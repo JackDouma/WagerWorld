a7c40013935076105eaaa20bf112341fbc0bd3c5
Jack <jackdouma0@gmail.com>
2025-01-15 16:33:13 -0500

    setup

--- src/page/header.jsx
+++ src/page/header.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './header.css'
+import '../css/header.css'
 
 function Header() {
 
--- src/root/Home.jsx
+++ src/root/Home.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Header from './header.jsx'
-import Index from './index.jsx'
+import '../css/index.css'
+import Header from '../page/header.jsx'
+import Index from '../page/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
