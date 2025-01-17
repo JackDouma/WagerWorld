a8353eb5a7e365d810e2aad6c25da8f2da35e98d
Michael Rosanelli <michaelrosanelli105@gmail.com>
2025-01-17 17:35:32 -0500

    basic blackjack

---
+++ Client/src/page/blackjack.jsx
@@ -0,0 +1,254 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Phaser from 'phaser'
+
+class BlackjackScene extends Phaser.Scene {
+
+  constructor() {
+    super({ key: 'BlackjackScene' })
+    this.cardScale = 0.25
+    this.deck = []
+    this.playerHand = []
+    this.dealerHand = []
+    this.isPlayerTurn = true
+    this.playerCardCount = 0
+    this.dealerCardCount = 0
+    // object used for later (cuz its gotta be played, then flipped over later instead of right away)
+    this.dealersSecond
+  }
+
+  preload() {
+    this.load.image('card', '/card-back.png')
+    const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+    const suits = ["spades", "clubs", "diamonds", "hearts"]
+
+    suits.forEach((suit) => {
+      values.forEach((value) => {
+        const fileName = `${value}_of_${suit}.png`
+        this.load.image(fileName, `/Cards/${fileName}`)
+      })
+    })
+  }
+
+  create() {
+    this.createDeck()
+    this.dealInitialCards()
+    this.createUI()
+  }
+
+  createDeck() {
+    const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+    const suits = ["spades", "clubs", "diamonds", "hearts"]
+    this.deck = suits.flatMap((suit) => values.map((value) => ({ suit, value })))
+    Phaser.Utils.Array.Shuffle(this.deck)
+    // scale is set to 0.25 because the actual .png files are huge
+    this.add.image(0, 0, 'card').setScale(this.cardScale)
+  }
+
+  dealInitialCards() {
+    this.playerHand = [this.deck.pop()]
+    this.playerHand.push(this.deck.pop())
+    this.dealerHand = [this.deck.pop()]
+    this.dealerHand.push(this.deck.pop())
+  }
+
+  createUI() {
+    const centerX = this.cameras.main.centerX
+    const centerY = this.cameras.main.centerY
+
+    this.add.text(centerX - 100, 50, 'Blackjack', { fontFamily: 'Arial', fontSize: '32px', fill: '#fff' })
+
+    this.hitButton = this.add
+      .text(centerX + 200, centerY - 75, 'Hit', { fontSize: '24px', fill: '#0f0' })
+      .setInteractive()
+      .on('pointerdown', () => this.hit())
+
+    this.standButton = this.add
+      .text(centerX + 190, centerY + 50, 'Stand', { fontSize: '24px', fill: '#f00' })
+      .setInteractive()
+      .on('pointerdown', () => this.stand())
+
+    // visually dealing out the cards
+    this.animateCard(100 + this.playerCardCount * 50, 400, `${this.playerHand[0].value}_of_${this.playerHand[0].suit}.png`, 0)
+    this.playerCardCount++
+    this.animateCard(100 + this.dealerCardCount * 50, 200, `${this.dealerHand[0].value}_of_${this.playerHand[0].suit}.png`, 1)
+    this.dealerCardCount++
+    this.animateCard(100 + this.playerCardCount * 50, 400, `${this.playerHand[1].value}_of_${this.playerHand[0].suit}.png`, 2)
+    this.playerCardCount++
+    this.dealersSecond = this.animateCard(100 + this.dealerCardCount * 50, 200, `card`, 3)
+    this.dealerCardCount++
+  }
+
+  animateCard(handX, handY, newTexture, order) {
+    // start by creating a card object in the top left of the screen
+    const card = this.add.image(0, 0, 'card').setScale(this.cardScale)
+  
+    // here we use tweens, the phaser way of doing animations
+    this.tweens.add({
+      // setting the object to be animated as the card we just made
+      targets: card,
+      // setting the destination for the card to end up
+      x: handX,
+      y: handY,
+      // how long the animation will take
+      duration: 500,
+      // how much time is spent waiting for it to happen (for at the start when 4 cards are dealt at the same time)
+      delay: order * 200,
+      // type of animation (idk, its what google said to use)
+      ease: 'Cubic.easeOut',
+      // do this once the animation is done
+      onComplete: () => {
+        // only do if the card is supposed to be flipped over (ie. the dealer's second card)
+        if (newTexture != 'card') {
+          // make another animation to make it flip over
+          this.tweens.add({
+            targets: card,
+            // by setting the horizontal scale to 0, we make it look like its mid-flip
+            scaleX: 0,
+            duration: 200,
+            ease: 'Linear',
+            // once its done we change the image used, and make it fully flip
+            onComplete: () => {
+              // Change the texture to the new card face
+              card.setTexture(newTexture)
+              // Flip back to full size
+              this.tweens.add({
+                targets: card,
+                // set horizontal scale back to what it was originally
+                scaleX: this.cardScale,
+                duration: 200,
+                ease: 'Linear',
+              })
+            },
+          })
+        }
+      },
+    })
+
+    return card
+  }
+
+  // manages all blackjack counting logic :)
+  calculateHandValue(hand) {
+    let value = 0
+    let aces = 0
+
+    hand.forEach((card) => {
+      if (['jack', 'queen', 'king'].includes(card.value)) {
+        value += 10
+      } else if (card.value === 'ace') {
+        value += 11
+        aces += 1
+      } else {
+        value += parseInt(card.value, 10)
+      }
+    })
+
+    while (value > 21 && aces > 0) {
+      value -= 10
+      aces -= 1
+    }
+
+    return value
+  }
+
+
+  hit() {
+    if (!this.isPlayerTurn) return
+
+    this.playerHand.push(this.deck.pop())
+    this.animateCard(100 + this.playerCardCount * 50, 400, `${this.playerHand[this.playerHand.length - 1].value}_of_${this.playerHand[this.playerHand.length - 1].suit}.png`, 0)
+    this.playerCardCount++
+
+    const playerValue = this.calculateHandValue(this.playerHand)
+    if (playerValue > 21)
+      this.endGame('Player Busts! Dealer Wins!')
+  }
+
+  stand() {
+    if (!this.isPlayerTurn) return
+
+    // another animation specifically for the dealer's second card
+    // essentially the same as the once used previously in animateCard()
+    this.tweens.add({
+      targets: this.dealersSecond,
+      scaleX: 0,
+      duration: 200,
+      ease: 'Linear',
+      onComplete: () => {
+        this.dealersSecond.setTexture(`${this.dealerHand[1].value}_of_${this.dealerHand[1].suit}.png`)
+        this.tweens.add({
+          targets: this.dealersSecond,
+          scaleX: this.cardScale,
+          duration: 200,
+          ease: 'Linear',
+        })
+      },
+    })
+    this.isPlayerTurn = false
+    this.dealerTurn()
+  }
+
+  dealerTurn() {
+    let dealerValue = this.calculateHandValue(this.dealerHand)
+
+    // dealer will draw cards until they get a total of at least 17
+    while (dealerValue < 17) {
+      this.dealerHand.push(this.deck.pop())
+      this.animateCard(100 + this.dealerCardCount * 50, 200, `${this.dealerHand[this.dealerHand.length - 1].value}_of_${this.dealerHand[this.dealerHand.length - 1].suit}.png`, 0)
+      this.dealerCardCount++
+      dealerValue = this.calculateHandValue(this.dealerHand)
+    }
+
+    this.checkWinner()
+  }
+
+  checkWinner() {
+    const playerValue = this.calculateHandValue(this.playerHand)
+    const dealerValue = this.calculateHandValue(this.dealerHand)
+
+    if (dealerValue > 21 || playerValue > dealerValue)
+      this.endGame('You Win!')
+    else if (playerValue < dealerValue)
+      this.endGame('You Lose...')
+    else
+      this.endGame("That's a Push")
+  }
+
+  endGame(message) {
+    this.add.text(this.cameras.main.centerX - 120, this.cameras.main.centerY + 225, message, {
+      fontSize: '32px',
+      fill: '#fff',
+    })
+    this.hitButton.disableInteractive()
+    this.standButton.disableInteractive()
+  }
+}
+
+// putting it all in a React component
+const BlackjackGame = () => {
+  const gameRef = useRef(null)
+  const [gameInstance, setGameInstance] = useState(null)
+
+  useEffect(() => {
+    const config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      backgroundColor: '#2d2d2d',
+      parent: 'phaser-game',
+      scene: BlackjackScene,
+    }
+
+    const game = new Phaser.Game(config)
+    gameRef.current = game
+    setGameInstance(game)
+
+    return () => {
+      game.destroy(true)
+    }
+  }, [])
+
+  return <div id="phaser-game"></div>
+}
+
+export default BlackjackGame
