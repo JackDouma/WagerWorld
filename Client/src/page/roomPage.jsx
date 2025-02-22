66440f5e062f5af186a63c295da89541da1679ef
Tyler Miles <thmiles@lakeheadu.ca>
2025-02-21 18:22:57 -0600

    Modified pages to use .env file for Colyseus server details

--- Client/src/page/roomPage.jsx
+++ Client/src/page/roomPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Client } from 'colyseus.js';
 
-const client = new Client('ws://localhost:2567'); // Replace with your Colyseus server URL
+const client = new Client(`ws://${import.meta.env.VITE_COLYSEUS_HOST}:${import.meta.env.VITE_COLYSEUS_PORT}`); // Replace with your Colyseus server URL
 
 function RoomPage() {
   const { roomId } = useParams();
