5b833e36003b30cfdac448e58296704414886b58
Chris Veilleux <82720132+chris-veilleux@users.noreply.github.com>
2025-03-04 16:02:22 -0500

    Install material UI and dependencies

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -28,6 +28,10 @@ import PokerById from './page/pokerbyid.jsx';
 import OrgRestrictedRoute from './component/OrgRestrictedRoute.jsx';  // Import OrgRestrictedRoute
 import "./styles.css";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
     return (
